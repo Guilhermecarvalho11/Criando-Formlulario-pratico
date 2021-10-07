5cd67d653a2cfb0ce9fce18ef6863a0d3cf02071
Guilhermecarvalho11 <carvalho_guilhermegomes@hotmail.com>
2021-10-07 15:36:42 -0300

    form

--- src/Controllers/UserController.js
+++ src/Controllers/UserController.js
@@ -1,8 +1,7 @@
 class UserController {
-    contructor(formId){
+    constructor(formId){
         this.form = document.getElementById(formId);
         this.onSubmit();
-        console.log("chegou aqui")
     }
 
     onSubmit(){
@@ -11,16 +10,39 @@ class UserController {
             this.getValues();
         })
     }
+message(user){
+    console.log(user);
+}
+
+    validation(element){
+        if(["name", "email", "phone", "password"].indexOf(element.name) > -1 && !element.value) {
+            element.classList.remove("success")
+            element.classList.add("error")
+            return false;
+        } else {
+            element.classList.remove("error")
+            element.classList.add("success")
+
+        }
+    }
 
     getValues(){
         let user = {};
+        let isvalid = true;
+
         [...this.form.elements].forEach((element, index) => {
+
+           isvalid = this.validation(element);
 
             if(element.name){
                 user[element.name] = element.value;
             }
         })
-        console.log(user)
 
+        if(!isvalid) {
+            return false;
+        }
+        let object = new User (user.name, user.email, user.phone, user.password)
+        this.message(object);
     }
 }
